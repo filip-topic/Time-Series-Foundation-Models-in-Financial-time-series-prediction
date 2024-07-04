4f2a1823c21567477cf7d6e7c7f500e0733b00bc
Filip Topic <filip.topic3@gmail.com>
2024-07-04 12:17:46 +0100

    first version of the notebook.ipynb file complete

--- modules/experiment/experiment.py
+++ modules/experiment/experiment.py
@@ -7,70 +7,60 @@ import os
 # Get the path of the MSc_dissertation directory
 base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Add the modules directory to the sys.path
-sys.path.append(os.path.join(base_dir, 'modules'))
+sys.path.append(base_dir)
 
 # Now you can import modules from the models folder
-from models import arima, lag_llama
-from data import data_loader, data_splitter, data_reader
+from modules.models import arima, lag_llama
+from modules.data import data_loader, data_splitter, data_reader
 
 def fill_results(name, results, y_true, y_pred):
     results["r2"][name].append(r2_score(y_true, y_pred))
     results["mse"][name].append(mean_squared_error(y_true, y_pred))
     results["mae"][name].append(mean_absolute_error(y_true, y_pred))
     results["rmse"][name].append(np.sqrt(mean_squared_error(y_true, y_pred)))
+    return results
 
 # takes multi-column data
 def run_experiment(data, models=["arima", "llama"], metrics=["r2", "mse", "mae", "rmse"], prediction_length=10):
-    
+
     results = {metric: {model: [] for model in models} for metric in metrics}
     
     first_col = True
     
+    i = 0
+
     for column in list(data.columns):
         if first_col:
             first_col = False
             continue
         
+        # this is here just to speed up testing
+        if i == 10:
+            break
+
+        i = i+1
         column_data = data[["ds", column]]
         column_data.columns = ["ds", "y"]
         
         train, test = data_splitter.split_data(column_data, prediction_length)
         
+        y_true = list(test["y"])
+
         if "arima" in models:
             arima_model = arima.get_autoarima(train)
             arima_predictions = arima.autoarima_predictions(arima_model, prediction_length)
+            y_pred_arima = arima_predictions
+            results = fill_results("arima", results, y_true, y_pred_arima)
+
         
         if "llama" in models:
             lag_llama_predictions, tss = lag_llama.get_lam_llama_forecast(train, prediction_length)
             lag_llama_predictions = list(lag_llama_predictions[0].samples.mean(axis=0))
-        
-        y_true = list(test["y"])
-        
-        if "arima" in models:
-            y_pred_arima = arima_predictions
-            results = fill_results("arima", results, y_true, y_pred_arima)
-        
-        if "llama" in models:
             y_pred_llama = lag_llama_predictions
-            results = fill_results("arima", results, y_true, y_pred_llama)
+            results = fill_results("llama", results, y_true, y_pred_llama)
             
         
-        print(column + " done")
+        print(column + f" done  {i}")
         print("------------")
-    
-    """
-    # Converting results to 2D list
-    output = []
-    for metric in metrics:
-        row = []
-        for column in list(data.columns):
-            if column == "ds":
-                continue
-            if "arima" in models:
-                row.append(results[metric]["arima"][data.columns.get_loc(column)-1])
-            if "llama" in models:
-                row.append(results[metric]["llama"][data.columns.get_loc(column)-1])
-        output.append(row)
-    """
     
     return results
