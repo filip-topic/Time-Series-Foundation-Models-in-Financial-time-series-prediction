e705b72096aa9ec22221c370bdb7a6b8458d6abf
Filip Topic <filip.topic3@gmail.com>
2024-07-29 13:34:44 +0100

    added the results writing function. Congregated all together in the notebook

---
+++ modules/sr/result_saver.py
@@ -0,0 +1,10 @@
+import os
+
+def save_results(df, file_name, type = "evaluation"):
+    project_folder = os.getcwd()
+    data_folder = os.path.abspath(os.path.join(project_folder, "results", type))
+    file_path = os.path.join(data_folder, file_name)
+    df.to_csv(file_path, index=False)
+    print(f"Data has been written to '{file_path}'.")
+    
+
