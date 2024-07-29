63ab29d92ab22bfd88002d34f2954792f29b4dd7
Filip Topic <filip.topic3@gmail.com>
2024-07-29 14:40:04 +0100

    started work on main.py

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from modules.evaluation.crps_interval import crps_time_series_interval
-from modules.evaluation.rmae import relative_mean_absolute_error
-from modules.evaluation.rrmse import relative_root_mean_square_error
-from modules.utils.arg_parser import parse_args_cmd
+# modules
+from modules.data import  data_reader, data_loader
+from modules.sr import result_saver
+from modules.models import lag_llama
+from modules.experiment.tscv import get_tscv_results, get_summary, extract_metrics
+from modules.visualization import graphs
+from modules.fine_tuning import lag_llama_ft
 
-args = parse_args_cmd()
-parsed_args = args.parse_args()
+# import libraries
+import itertools
+
+# parameters
+PREDICTION_LENGTH = 1 
+TICKER = "AAPL"
+FREQUENCY = ["minutely", "hourly", "daily"] 
+TYPE_OF_DATA = ["stock", "return"] 
+MODELS = ["arima", "llama", "autoregressor", "fine-tuned Llama"] 
+FOLDS = 10 
+CONTEXT_LENGTH = [32, 64, 128]
+METRICS = ['r2', 'mse', 'mae', 'rmse', 'mda', "mape"]
+
+# fine-tuning parameters
+BATCH_SIZE = 10
+MAX_EPOCHS = 20
+
+# data parameters
+FT_START_DATE = "2022-07-07"
+START_DATE = "2023-07-07"
+END_DATE = "2024-07-07"
+
+# want to add
+#TRAIN_PERIOD = # context lenghts. Should take a look into this
+TRAIN_SIZE = CONTEXT_LENGTH
 
---
+++ modules/experiment/run_experiment.py
@@ -0,0 +1,68 @@
+from modules.data import  data_reader, data_loader
+from modules.sr import result_saver
+from modules.models import lag_llama
+from modules.experiment.tscv import get_tscv_results, get_summary, extract_metrics
+from modules.visualization import graphs
+from modules.fine_tuning import lag_llama_ft
+
+def save_results(prediction_length,
+                ticker, 
+                frequency, 
+                type_of_data,
+                folds,
+                context_length,
+                batch_size,
+                max_epochs,
+                ft_start_date,
+                start_date,
+                end_date):
+    
+    # data config
+    data_config = {"ticker" : ticker,
+               "frequency" : frequency,
+               "start" : start_date,
+               "end" : end_date}
+
+    ft_data_config = {"ticker" : ticker,
+                     "frequency" : frequency,
+                     "start" : ft_start_date,
+                     "end" : start_date}
+    
+    # loading the data
+    data = data_loader.get_data(data_type=type_of_data, kwargs=data_config)
+    ft_data = data_loader.get_data(data_type=type_of_data, kwargs=ft_data_config)
+
+    data_length = len(data)
+    ft_length = len(ft_data)
+
+    # fine tuning Lag Llama - preparing the training data
+    ft_train_data = lag_llama.prepare_data(data=ft_data, 
+                                       prediction_length=0, 
+                                       frequency=frequency)
+    
+    # fine tuning Lag Llama - creating the predictor object
+    predictor = lag_llama_ft.get_predictor(prediction_length=prediction_length, 
+                                       context_length=context_length, 
+                                       batch_size=batch_size, 
+                                       max_epochs=max_epochs)
+    
+    # fine tuning Lag Llama - fine-tuning the predictor object
+    predictor = predictor.train(ft_train_data, 
+                            cache_data = True, 
+                            shuffle_buffer_length = 1000)
+    
+    # getting the TSCV results
+    r, p = get_tscv_results(data = data,
+                           prediction_horizon=prediction_length,
+                           context_length=context_length, 
+                           folds=folds, 
+                           frequency=frequency,
+                           predictor=predictor)
+    
+    # experiment name
+    experiment_name = f"P_L={prediction_length}__T={ticker}__FR={frequency}__T_O_D={type_of_data}__FO={folds}__C_L_T_S={context_length}__FT_S_D={ft_start_date}__S_D={start_date}__E_D={end_date}__FT_L={ft_length}__D_L={data_length}.csv"
+
+    # saving the results
+    result_saver.save_results(r, experiment_name, type="evaluation")
+    result_saver.save_results(p, experiment_name, type="prediction")    
+
